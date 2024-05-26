c9fe3e20792f90c41582d3ebe17b89f8ce8e769c
Ashil Thomas <ashilthomas31@gmail.com>
2024-05-27 01:07:21 +0530

    razorpay payment methode added

--- src/Pages/BookDetailes/BookDetailes.jsx
+++ src/Pages/BookDetailes/BookDetailes.jsx
@@ -1,148 +1,5 @@
 
-// import React, { useContext, useState } from "react";
-// import { Col, Row } from "react-bootstrap";
-// import "./BookDetailes.css";
-// import Recomended from "../../Components/Recomended/Recomended";
-// import { useParams } from "react-router-dom";
-// import { StoreContext } from "../../Context/StoreContext";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import axios from "axios";
-
-// function BookDetailes() {
-//   const { id } = useParams();
-//   const { product,getAllCart } = useContext(StoreContext);
-//   const [quantity, setquantity] = useState(1);
-  
-
-
-  
-
-
- 
-//   const handleAddToCart = async (productId) => {
-   
-//     try {
-//       const res = await axios.post("http://localhost:3003/cart/addcart", {
-//         quantity,
-//         productId:productId,
-    
-//       },{withCredentials:true});
-//       getAllCart()
-    
-//       if (res.data.success) {
-//         toast(res.data.message);
-//       } else {
-//         toast(res.data.message);
-//       }
-//     } catch (error) {
-//       console.error("Error adding product to cart:", error);
-
-//       toast("already added to product");
-//     }
-//   };
-
-//   const current = product.find((item) => item._id === id);
-
-//   const paymentHandler = async (event, courseId) => {
-//     const selectedCourse = product.find((item) => item._id === courseId);
-//     if (!selectedCourse) return;
-//     const response = await axios.post(
-//       "http://localhost:3003/payment/order",
-//       { amount: selectedCourse.price },
-//     );
-
-//     const order = await response.data.data;
-//     console.log(order);
-//     const option = {
-//       key: import.meta.env.VITE_SOME_KEY,
-//       amount: order.amount,
-//       currency: order.currency,
-//       name: "Anu Codder",
-//       description: "Test Transaction",
-//       image: "https://i.ibb.co/5Y3m33n/test.png",
-//       order_id: order.id,
-//       handler: async function (response) {
-//         const body = { ...response };
-
-//         const validateResponse = await axios.post(
-//           "http://localhost:3003/payment/verify",
-//           body,
-//         );
-
-//         const jsonResponse = await validateResponse;
-
-//         console.log("jsonResponse", jsonResponse);
-//       },
-//       prefill: {
-//         name: "Anu Coder",
-//         email: "anucoder@example.com",
-//         contact: "00000000",
-//       },
-//       notes: {
-//         address: "Razorpay Corporate Office",
-//       },
-//       theme: {
-//         color: "#3399cc",
-//       },
-//     };
-
-//     const rzp1 = new window.Razorpay(option);
-
-//     rzp1.on("payment.failed", function (response) {
-//       alert(response.error.code);
-//     });
-
-//     rzp1.open();
-//     event.preventDefault();
-//   };
-
-
-//   return (
-//     <div className="section">
-//       <ToastContainer />
-//       <div className="container-detaile-books">
-//         <Row>
-//           <Col className="col-12 col-md-6 ">
-//             {current && (
-//               <img
-//                 src={`http://localhost:3003/${current.image}`}
-//                 className="product-detaile-img"
-//                 alt=""
-//               />
-//             )}
-//           </Col>
-//           <Col className="col-12 col-md-6 product-details">
-//             {current && (
-//               <>
-//                 <h2>{current.title}</h2>
-//                 <h3>{current.author}</h3>
-//                 <p>{current.price}</p>
-//                 <p>
-//                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-//                   tempor est sit amet purus luctus, ac mattis odio fermentum.
-//                   Vestibulum ante ipsum primis in faucibus orci luctus et
-//                   ultrices posuere cubilia Curae; Mauris nec ultricies dui.
-//                 </p>
-//                 <hr />
-//                 <div className="product-btn">
-                 
-//                   <button onClick={() => handleAddToCart(current._id)}>Add to Cart</button>
-//                   <button onClick={(event)=>paymentHandler(event,current._id)}>buy</button>
-//                 </div>
-//                 <hr />
-//               </>
-//             )}
-//           </Col>
-//         </Row>     
-//       </div>
-//  <Recomended />
-//     </div>
-//   );
-// }
-
-// export default BookDetailes;
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import "./BookDetailes.css";
 import Recomended from "../../Components/Recomended/Recomended";
@@ -154,103 +11,101 @@ import axios from "axios";
 
 function BookDetailes() {
   const { id } = useParams();
-  const { product, getAllCart } = useContext(StoreContext);
-  const [quantity, setQuantity] = useState(1);
+  const { product,getAllCart } = useContext(StoreContext);
+  const [quantity, setquantity] = useState(1);
+  
 
+
+  
+
+
+ 
   const handleAddToCart = async (productId) => {
+   
     try {
-      const res = await axios.post(
-        "http://localhost:3003/cart/addcart",
-        {
-          quantity,
-          productId: productId,
-        },
-        { withCredentials: true }
-      );
-      getAllCart();
-
-      toast(res.data.message);
+      const res = await axios.post("http://localhost:3003/cart/addcart", {
+        quantity,
+        productId:productId,
+    
+      },{withCredentials:true});
+      getAllCart()
+    
+      if (res.data.success) {
+        toast(res.data.message);
+      } else {
+        toast(res.data.message);
+      }
     } catch (error) {
       console.error("Error adding product to cart:", error);
-      toast("Already added to product");
+
+      toast("already added to product");
     }
   };
 
   const current = product.find((item) => item._id === id);
 
-  const paymentHandler = async (event, courseId) => {
-    event.preventDefault();
-    const selectedCourse = product.find((item) => item._id === courseId);
+  const paymentHandler = async (event, productId) => {
+    const selectedCourse = product.find((item) => item._id === productId);
     if (!selectedCourse) return;
+    const response = await axios.post(
+      "http://localhost:3003/payment/order",
+      { amount: selectedCourse.price ,productId},
+    );
 
-    try {
-      const response = await axios.post(
-        "http://localhost:3003/payment/order",
-        { amount: selectedCourse.price }
-      );
+    const order = await response.data.data;
+   
+    const option = {
+      key: import.meta.env.VITE_SOME_KEY,
+      amount: order.amount,
+      currency: order.currency,
+      name: "Anu Codder",
+      description: "Test Transaction",
+      image: "https://i.ibb.co/5Y3m33n/test.png",
+      order_id: order.id,
+     
+      handler: async function (response) {
+        const body = { ...response,productId };
 
-      const order = response.data.data;
-      const options = {
-        key: import.meta.env.VITE_SOME_KEY,
-        amount: order.amount,
-        currency: order.currency,
-        name: "Anu Codder",
-        description: "Test Transaction",
-        image: "https://i.ibb.co/5Y3m33n/test.png",
-        order_id: order.id,
-        handler: async function (response) {
-          const body = { ...response };
-          const validateResponse = await axios.post(
-            "http://localhost:3003/api/v1/payment/verify",
-            body
-          );
+        const validateResponse = await axios.post(
+          "http://localhost:3003/payment/verify",
+          body,
+        );
 
-          console.log("jsonResponse", validateResponse);
-        },
-        prefill: {
-          name: "Anu Coder",
-          email: "anucoder@example.com",
-          contact: "00000000",
-        },
-        notes: {
-          address: "Razorpay Corporate Office",
-        },
-        theme: {
-          color: "#3399cc",
-        },
-      };
+        const jsonResponse = await validateResponse;
 
-      if (window.Razorpay) {
-        const rzp1 = new window.Razorpay(options);
+        console.log("jsonResponse", jsonResponse);
+      },
+      prefill: {
+        name: "Anu Coder",
+        email: "anucoder@example.com",
+        contact: "00000000",
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
 
-        rzp1.on("payment.failed", function (response) {
-          alert(response.error.code);
-        });
 
-        rzp1.open();
-      } else {
-        console.error("Razorpay SDK not loaded");
-      }
-    } catch (error) {
-      console.error("Error in payment handler:", error);
-    }
+    const rzp1 = new window.Razorpay(option);
+
+    rzp1.on("payment.failed", function (response) {
+      alert(response.error.code);
+    });
+
+    rzp1.open();
+    event.preventDefault();
   };
 
-  useEffect(() => {
-    if (!window.Razorpay) {
-      const script = document.createElement("script");
-      script.src = "https://checkout.razorpay.com/v1/checkout.js";
-      script.async = true;
-      document.body.appendChild(script);
-    }
-  }, []);
 
   return (
     <div className="section">
       <ToastContainer />
       <div className="container-detaile-books">
         <Row>
-          <Col xs={12} md={6}>
+          <Col className="col-12 col-md-6 ">
             {current && (
               <img
                 src={`http://localhost:3003/${current.image}`}
@@ -259,7 +114,7 @@ function BookDetailes() {
               />
             )}
           </Col>
-          <Col xs={12} md={6} className="product-details">
+          <Col className="col-12 col-md-6 product-details">
             {current && (
               <>
                 <h2>{current.title}</h2>
@@ -273,18 +128,184 @@ function BookDetailes() {
                 </p>
                 <hr />
                 <div className="product-btn">
+                 
                   <button onClick={() => handleAddToCart(current._id)}>Add to Cart</button>
-                  <button onClick={(event) => paymentHandler(event, current._id)}>Buy</button>
+                  <button onClick={(event)=>paymentHandler(event,current._id)}>buy</button>
                 </div>
                 <hr />
               </>
             )}
           </Col>
-        </Row>
+        </Row>     
       </div>
-      <Recomended />
+ <Recomended />
     </div>
   );
 }
 
 export default BookDetailes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useContext, useState, useEffect } from "react";
+// import { Col, Row } from "react-bootstrap";
+// import "./BookDetailes.css";
+// import Recomended from "../../Components/Recomended/Recomended";
+// import { useParams } from "react-router-dom";
+// import { StoreContext } from "../../Context/StoreContext";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import axios from "axios";
+
+// function BookDetailes() {
+//   const { id } = useParams();
+//   const { product, getAllCart } = useContext(StoreContext);
+//   const [quantity, setQuantity] = useState(1);
+
+//   const handleAddToCart = async (productId) => {
+//     try {
+//       const res = await axios.post(
+//         "http://localhost:3003/cart/addcart",
+//         {
+//           quantity,
+//           productId: productId,
+//         },
+//         { withCredentials: true }
+//       );
+//       getAllCart();
+
+//       toast(res.data.message);
+//     } catch (error) {
+//       console.error("Error adding product to cart:", error);
+//       toast("Already added to product");
+//     }
+//   };
+
+//   const current = product.find((item) => item._id === id);
+
+//   const paymentHandler = async (event, courseId) => {
+//     event.preventDefault();
+//     const selectedCourse = product.find((item) => item._id === courseId);
+//     if (!selectedCourse) return;
+
+//     try {
+//       const response = await axios.post(
+//         "http://localhost:3003/payment/order",
+//         { amount: selectedCourse.price }
+//       );
+
+//       const order = response.data.data;
+//       const options = {
+//         key: import.meta.env.VITE_SOME_KEY,
+//         amount: order.amount,
+//         currency: order.currency,
+//         name: "Anu Codder",
+//         description: "Test Transaction",
+//         image: "https://i.ibb.co/5Y3m33n/test.png",
+//         order_id: order.id,
+//         handler: async function (response) {
+//           const body = { ...response };
+//           const validateResponse = await axios.post(
+//             "http://localhost:3003/api/v1/payment/verify",
+//             body
+//           );
+
+//           console.log("jsonResponse", validateResponse);
+//         },
+//         prefill: {
+//           name: "Ashil Coder",
+//           email: "ashilr@example.com",
+//           contact: "00000000",
+//         },
+//         notes: {
+//           address: "Razorpay Corporate Office",
+//         },
+//         theme: {
+//           color: "#3399cc",
+//         },
+//       };
+
+//       if (window.Razorpay) {
+//         const rzp1 = new window.Razorpay(options);
+
+//         rzp1.on("payment.failed", function (response) {
+//           alert(response.error.code);
+//         });
+
+//         rzp1.open();
+//       } else {
+//         console.error("Razorpay SDK not loaded");
+//       }
+//     } catch (error) {
+//       console.error("Error in payment handler:", error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (!window.Razorpay) {
+//       const script = document.createElement("script");
+//       script.src = "https://checkout.razorpay.com/v1/checkout.js";
+//       script.async = true;
+//       document.body.appendChild(script);
+//     }
+//   }, []);
+
+//   return (
+//     <div className="section">
+//       <ToastContainer />
+//       <div className="container-detaile-books">
+//         <Row>
+//           <Col xs={12} md={6}>
+//             {current && (
+//               <img
+//                 src={`http://localhost:3003/${current.image}`}
+//                 className="product-detaile-img"
+//                 alt=""
+//               />
+//             )}
+//           </Col>
+//           <Col xs={12} md={6} className="product-details">
+//             {current && (
+//               <>
+//                 <h2>{current.title}</h2>
+//                 <h3>{current.author}</h3>
+//                 <p>{current.price}</p>
+//                 <p>
+//                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+//                   tempor est sit amet purus luctus, ac mattis odio fermentum.
+//                   Vestibulum ante ipsum primis in faucibus orci luctus et
+//                   ultrices posuere cubilia Curae; Mauris nec ultricies dui.
+//                 </p>
+//                 <hr />
+//                 <div className="product-btn">
+//                   <button onClick={() => handleAddToCart(current._id)}>Add to Cart</button>
+//                   <button onClick={(event) => paymentHandler(event, current._id)}>Buy</button>
+//                 </div>
+//                 <hr />
+//               </>
+//             )}
+//           </Col>
+//         </Row>
+//       </div>
+//       <Recomended />
+//     </div>
+//   );
+// }
+
+// export default BookDetailes;
---
+++ src/Pages/UserDetails/UserDetails.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function UserDetails() {
+  return (
+   <div>
+    <div className="contianer">
+        <Row>
+            <Col className="col-md-6">
+            </Col>
+            <Col className="col-md-6">
+                <div className="order-items">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+
+                </div>
+            </Col>
+           
+        </Row>
+    </div>
+   </div>
+  )
+}
+
+export default UserDetails
